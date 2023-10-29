d27d3a0aa36aaf025c762d1e031fd60b812ce723
Willian <willianf4m@gmail.com>
2023-10-29 18:31:39 -0300

    feat: created migration files with the db structure

---
+++ database/migrations/2023_10_16_234232_create_movie_producer_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movie_producer', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('producer_id')
+                ->references('id')
+                ->on('producers')
+                ->cascadeOnDelete();
+            $table->foreignId('movie_id')
+                ->references('id')
+                ->on('movies')
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movie_producer');
+    }
+};
